7bc8588d4eb724966bb8577b58a2c3917f1c864f
Ann Maria <329204228+annmaria5804-2004@users.noreply.github.com>
2026-09-16 23:31:23 +0530

    Secure Django secret key

--- ISLtranslator/settings.py
+++ ISLtranslator/settings.py
@@ -21,8 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@m_4@h6fa1=*%n285k5##657cd)u#znpzparxbg@z9so=t5fhc'
-
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "change-this-secret-key")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
